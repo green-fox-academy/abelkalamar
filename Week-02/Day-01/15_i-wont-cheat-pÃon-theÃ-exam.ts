b7b02e15305d8d4d6de46fdd398458c184f61fae
Abel Kalamar <abel.kalamar@gmail.com>
2018-10-29 12:22:04 +0100

    add print-even.ts

---
+++ Week-02/Day-01/15_i-wont-cheat-pÃon-theÃ-exam.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+let statement: string = "I won't cheat on the exam!";
+
+for (let i: number = 0; i < 100; i ++){
+    console.log(statement);
+}
