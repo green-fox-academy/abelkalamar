7ddfe14055d8c74d0087873b9185651e176ebf85
Abel Kalamar <abel.kalamar@gmail.com>
2018-10-29 14:23:16 +0100

    add draw-pyramid.ts

---
+++ Week-02/Day-01/20_draw-a-pÃyramid.ts
@@ -0,0 +1,26 @@
+'use strict';
+export {}
+
+let lineCount: number = 30;
+
+// Write a program that draws a
+// pyramid like this:
+//
+//    *
+//   ***
+//  *****
+// *******
+//
+// The pyramid should have as many lines as lineCount is
+
+let star: string = "*";
+let spaceNum: number = lineCount
+
+
+for ( let i:number = 1; i <= lineCount; i ++){
+
+    let spc: string = (" ").repeat(spaceNum);
+    console.log(spc + star);
+    star += "**";
+    spaceNum --;
+}
