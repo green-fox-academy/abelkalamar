33a19d7de08cbecf030340403dab0ffc42b4eabb
Abel Kalamar <abel.kalamar@gmail.com>
2018-10-29 11:05:20 +0100

    add basic-info.ts

---
+++ Week-02/Day-01/10_basic-info.ts
@@ -0,0 +1,25 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+
+// Your name as a string
+
+let myName: string = "Abel Kalamar";
+
+// Your age as an integer
+
+let myAge: number = 25;
+
+// Your height in meters as a float
+
+let myHeight: number = 1.73;
+
+// Whether you are married or not as a boolean
+
+let amIMarried: boolean = false;
+
+console.log(myName);
+console.log(myAge);
+console.log(myHeight);
+console.log(amIMarried);
+
