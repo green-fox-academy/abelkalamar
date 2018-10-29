b21151d3296f55162aad2e79751cb457c0dbe906
Abel Kalamar <abel.kalamar@gmail.com>
2018-10-29 16:14:54 +0100

    add draw-chesstable.ts

---
+++ Week-02/Day-01/25_draw_chesstable.ts
@@ -0,0 +1,27 @@
+export {}
+
+'use strict';
+
+// Create a program that draws a chess table like this
+//
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+//
+
+let obj: string = "% ";
+let lineCount: number = 10;
+let space: string = " ";
+
+for (let i: number = 1; i <= lineCount; i ++) {
+    if( i % 2 == 0){
+        console.log(obj.repeat(5));
+    } else {
+        console.log(space + obj.repeat(5));
+    }
+}
