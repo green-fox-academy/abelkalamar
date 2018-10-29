fd2805010cbd6e5d813ddb6eebaa7ab600f62c90
Abel Kalamar <abel.kalamar@gmail.com>
2018-10-29 10:37:40 +0100

    add introduction.ts

---
+++ Week-02/Day-01/04_introducing.ts
@@ -0,0 +1,13 @@
+"use strict"
+
+// Write a program that prints a few details to the console about you
+// It should print each detail to a new line.
+
+let myName = "Abel Kalamar";
+let myAge = 25;
+let myHeight = 1.73 
+
+// Output
+console.log(myName);
+console.log(myAge);
+console.log(myHeight);
