d74d344ac0657756768568e5ce37e4fa3139f5c6
Abel Kalamar <abel.kalamar@gmail.com>
2018-10-30 14:37:45 +0100

    rewrite double items.ts

--- Week-02/Day-02/14_append-a.ts
+++ Week-02/Day-02/14_append-a.ts
@@ -1,3 +1,5 @@
+"use stricts"
+
 // -  Create an array variable named `animals`
 //    with the following content: `["koal", "pand", "zebr"]`
 // -  Add all elements an `"a"` at the end
--- Week-02/Day-02/15_double-items.ts
+++ Week-02/Day-02/15_double-items.ts
@@ -1,12 +1,16 @@
+"use strict"
+
 // -  Create an array variable named `drinks` with the following content: `['Gin', 'Whiskey', 'Wine', 'Beer']`
 // -  Double all the strings in the array, use a built in array method instead of a loop
 // It should print: ['GinGin', 'WhiskeyWhiskey', 'WineWine', 'BeerBeer']`
 
 let drinks: string[] = ["Gin", "Whiskey", "Wine", "Beer"];
 
-let doubleDrinks = drinks.map(function(e){
-    return e += e;
-
+drinks.forEach((element, index, array)=> {
+    array[index] = element + element;
 })
 
-console.log(doubleDrinks);
+console.log(drinks);
+
+
+
