e95f2cb5c41affca5baff276d2e528a8fd6a403c
Abel Kalamar <abel.kalamar@gmail.com>
2018-10-30 11:04:15 +0100

    re-write for loop in print-param.ts

--- Week-02/Day-02/06_print-param.ts
+++ Week-02/Day-02/06_print-param.ts
@@ -2,8 +2,11 @@
 //    which logs to the console the input parameters
 //    (can have multiple number of arguments)
 
-function printParams(...nums: number[]){
-    console.log(nums);
+function printParams(...nums){
+    for (let i: number = 0; i < nums.length; i ++){
+        console.log(nums[i]);
+    }
+    
 }
 
 printParams(56, 77, 888,34, 54);
