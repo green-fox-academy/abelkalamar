6629919b861bdde0da7f0d3119bca1edb262badd
Abel Kalamar <abel.kalamar@gmail.com>
2018-10-30 16:43:53 +0100

    add reverse.ts

---
+++ Week-02/Day-02/19_reverse.ts
@@ -0,0 +1,32 @@
+"use strict"
+export{}
+
+// -  Create a variable named `numList`
+//    with the following content: `[3, 4, 5, 6, 7]`
+// -  Reverse the order of the elements in `numList`
+// 	   -  do it with the built in method
+
+let numList: number[] = [3, 4, 5, 6, 7];
+
+function reverse(array: number[]){
+        array.splice(0, 5, array[4], array[3], array[2], array[1], array[0]);
+        return array;
+    }
+    
+    console.log(reverse(numList));
+
+
+// console.log(numList.reverse());
+
+//	   -  do it with creating a new temp array and a loop
+// -  Print the elements of the reversed `numList`
+
+
+let newArray: number[] = numList.reverse();
+
+for (let i: number = 0; i < numList.length; i++) {
+    numList[i] = (newArray[i]);
+    
+}
+
+console.log(numList);
