739820d483ef311bbaf288c33141977a3adb27cd
Abel Kalamar <abel.kalamar@gmail.com>
2018-10-30 10:35:12 +0100

    add print-param.ts

---
+++ Week-02/Day-02/06_print-param.ts
@@ -0,0 +1,9 @@
+// -  Create a function called `printParams`
+//    which logs to the console the input parameters
+//    (can have multiple number of arguments)
+
+function printParams(nums: number[]){
+    console.log(nums);
+}
+
+printParams([5,6,7,8]);
