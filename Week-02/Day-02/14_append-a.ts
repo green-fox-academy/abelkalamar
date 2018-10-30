67150643ea858c90f7f90326c2115731963d7390
Abel Kalamar <abel.kalamar@gmail.com>
2018-10-30 14:06:15 +0100

    add append-a.ts

---
+++ Week-02/Day-02/14_append-a.ts
@@ -0,0 +1,12 @@
+// -  Create an array variable named `animals`
+//    with the following content: `["koal", "pand", "zebr"]`
+// -  Add all elements an `"a"` at the end
+
+let animals: string[] = ["koal", "pand", "zebr"];
+
+let aanimals = animals.map(function(e){
+    return e += "a";
+
+})
+
+console.log(aanimals);
