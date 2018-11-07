063cf00b11bfcbab67dc851787af049f12cc82d7
Abel Kalamar <abel.kalamar@gmail.com>
2018-11-07 17:33:21 +0100

    rewrite connect-the-dots-ts

--- Week-03/Day-03/Connect-dots/connect-the-dots-ts.ts
+++ Week-03/Day-03/Connect-dots/connect-the-dots-ts.ts
@@ -12,19 +12,15 @@ const ctx = canvas.getContext('2d');
 // Connect these: [[50, 100], [70, 70], [80, 90], [90, 90], [100, 70],
 // [120, 100], [85, 130], [50, 100]]
 
-let dots1: number[][] = [[10, 10], [290,  10], [290, 290], [10, 290], [10, 10]];
+let dots1: number[][] = [[10, 10], [290, 10], [290, 290], [10, 290], [10, 10]];
 let dots2: number[][] = [[50, 100], [70, 70], [80, 90], [90, 90], [100, 70], [120, 100], [85, 130], [50, 100]];
 
 function connectDots(arr: number[][]) {
   ctx.strokeStyle = "green";
   ctx.beginPath();
-  let startX: number = arr[0][0];
-  let startY: number = arr[0][1];
-  ctx.moveTo(startX, startY);
-  for (let i: number = 1; i <= arr.length; i++){
-    let toX: number = arr[i][0];
-    let toY: number = arr[i][1];
-    ctx.lineTo(toX, toY);
+  ctx.moveTo(arr[0][0], arr[0][1]);
+  for (let i: number = 1; i <= arr.length; i++) {
+    ctx.lineTo(arr[i][0], arr[i][1]);
     ctx.stroke();
   }
 }
