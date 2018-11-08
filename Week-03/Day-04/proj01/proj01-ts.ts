4397aab7733728f9f6ee2bccb21b8968efc5f890
Abel Kalamar <abel.kalamar@gmail.com>
2018-11-08 23:36:34 +0100

    move and rename files

--- Week-03/Day-04/proj01/proj01-ts.ts
+++ Week-03/Day-04/proj01/proj01-ts.ts
@@ -6,7 +6,7 @@ const ctx = canvas.getContext('2d');
 ctx.fillStyle = 'white';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-export function drawIt(lineDistance: number, color: string, direction: number): void {
+export function drawLines(lineDistance: number, color: string, direction: number): void {
   ctx.strokeStyle = color;
   ctx.beginPath();
   for (let i: number = 0; i < canvas.width; i += lineDistance) {
@@ -20,5 +20,5 @@ export function drawIt(lineDistance: number, color: string, direction: number): 
   }
   ctx.stroke();
 }
-drawIt(30, 'purple', 1);
-drawIt(30, 'green', 2);
+drawLines(30, 'purple', 1);
+drawLines(30, 'green', 2);
