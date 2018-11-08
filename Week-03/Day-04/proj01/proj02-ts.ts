03a5f11bf944fafa7b64d89d8305764205e83994
Abel Kalamar <abel.kalamar@gmail.com>
2018-11-08 22:15:25 +0100

    change the file's name

--- Week-03/Day-04/proj01/proj02-ts.ts
+++ Week-03/Day-04/proj01/proj02-ts.ts
@@ -3,12 +3,14 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
+import { drawLines } from ../proj01/proj01;
 ctx.fillStyle = 'white';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.scale(0.5, 0.5);
 
-export function drawIt(lineDistance: number, color: string, direction: number): void {
-  ctx.strokeStyle = color;
+function drawLines(lineDistance: number, color: string, direction: number): void {
   ctx.beginPath();
+  ctx.strokeStyle = color;
   for (let i: number = 0; i < canvas.width; i += lineDistance) {
     if (direction === 1) {
       ctx.moveTo(i, 0);
@@ -20,5 +22,17 @@ export function drawIt(lineDistance: number, color: string, direction: number): 
   }
   ctx.stroke();
 }
-drawIt(30, 'purple', 1);
-drawIt(30, 'green', 2);
+
+
+
+//function multyLines
+
+drawLines(30, 'green', 1);
+drawLines(30, 'purple', 2);
+
+/* else {
+  ctx.moveTo(0, i);
+  ctx.lineTo(i, canvas.height);
+}*/
+ctx.translate(500, 500);
+ctx.rotate(Math.PI);
