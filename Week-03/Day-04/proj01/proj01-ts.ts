66489f7f89560cef083d5f6a536a4bed23368ac5
Abel Kalamar <abel.kalamar@gmail.com>
2018-11-08 21:49:36 +0100

    feat(proj01): shorten the code

--- Week-03/Day-04/proj01/proj01-ts.ts
+++ Week-03/Day-04/proj01/proj01-ts.ts
@@ -6,23 +6,19 @@ const ctx = canvas.getContext('2d');
 ctx.fillStyle = 'white';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-function drawIt(startX: number, startY: number, lineDistance: number, color: string, direction: number): void {
+function drawIt(lineDistance: number, color: string, direction: number): void {
   ctx.strokeStyle = color;
   ctx.beginPath();
   for (let i: number = 0; i < canvas.width * 2; i += lineDistance) {
     if (direction === 1) {
-      ctx.moveTo(startX, 0);
-      ctx.lineTo(canvas.width, startY);
-      startX += lineDistance;
-      startY += lineDistance;
+      ctx.moveTo(i, 0);
+      ctx.lineTo(canvas.width, i);
     } else {
-      ctx.moveTo(0, startY);
-      ctx.lineTo(startX, canvas.height);
-      startX += lineDistance;
-      startY += lineDistance;
+      ctx.moveTo(0, i);
+      ctx.lineTo(i, canvas.height);
     }
   }
   ctx.stroke();
 }
-drawIt(0, 0, 30, 'purple', 1);
-drawIt(0, 0, 30, 'green', 2);
+drawIt(30,'purple', 1);
+drawIt(30, 'green', 2);
