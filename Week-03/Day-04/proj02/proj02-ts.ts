b0ff16b51219e0e6a9c462face496d805a6a4d00
Abel Kalamar <abel.kalamar@gmail.com>
2018-11-08 17:41:07 +0100

    feat(proj02): modify drawing function /still not working properly

--- Week-03/Day-04/proj02/proj02-ts.ts
+++ Week-03/Day-04/proj02/proj02-ts.ts
@@ -7,27 +7,25 @@ ctx.fillStyle = 'white';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 
-function drawIt(startX: number, startY: number, lineDistance: number, color: string, direction: number): void {
+function drawIt(lineDistance: number, color: string, direction: number): void {
   ctx.strokeStyle = color;
   ctx.beginPath();
+  let startX: number = 0;
+  let startY: number = 0;
   let parts: number = 4;
-  let newWidth = canvas.width / parts;
-  let newHeight = canvas.height / parts;
-  let newLineDist = lineDistance / parts;  
-  for (let i: number = 0; i < canvas.width * 2; i += lineDistance) {
+  let newWidth = canvas.width / parts * 2;
+  let newHeight = canvas.height / parts * 2;
+  let newLineDist = lineDistance / parts * 2;  
+  for (let i: number = 0; i < newWidth; i += newLineDist) {
     if (direction === 1) {
-      ctx.moveTo(startX, 0);
-      ctx.lineTo(canvas.width, startY);
-      startX += lineDistance;
-      startY += lineDistance;
+      ctx.moveTo(i, 0);
+      ctx.lineTo(newWidth, i);
     } else {
-      ctx.moveTo(0, startY);
-      ctx.lineTo(startX, canvas.height);
-      startX += lineDistance;
-      startY += lineDistance;
+    ctx.moveTo(0, i);
+    ctx.lineTo(i, newHeight);
     }
   }
   ctx.stroke();
 }
-drawIt(0, 0, 30, 'purple', 1);
-drawIt(0, 0, 30, 'green', 2);
+drawIt(30, 'purple', 1);
+drawIt(30, 'green', 2);
