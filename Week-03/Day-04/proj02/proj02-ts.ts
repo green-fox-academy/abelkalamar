ff107115e9100e108587a5e7d608b953b142c2e8
Abel Kalamar <abel.kalamar@gmail.com>
2018-11-09 13:13:56 +0100

    rewrite some lines in proj02

--- Week-03/Day-04/proj02/proj02-ts.ts
+++ Week-03/Day-04/proj02/proj02-ts.ts
@@ -3,8 +3,11 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = 'white';
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+// just for white background
+//ctx.fillStyle = 'white';
+//ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+// draw the lines
 
 function drawLines(lineDistance: number, color: string, direction: number): void {
   ctx.beginPath();
@@ -25,18 +28,16 @@ function multyLines(num: number, color1: string, color2: string, lineDistance: n
   ctx.scale(1 / num, 1 / num);
   let w: number = canvas.width;
   let h: number = canvas.height;
-  let y: number = 0;
-  let x: number = 0;
   for (let i: number = 0; i < num; i++) {
     for (let j: number = 0; j < num; j++) {
       drawLines(lineDistance, color1, 1);
       drawLines(lineDistance, color2, 2);
-      ctx.translate(w, y);
+      ctx.translate(w, 0);
     }
     ctx.translate(- num * w, h);
   }
 }
 
-multyLines(4, 'green', 'purple', 30);
+multyLines(6, 'blue', 'green', 30);
 
 
