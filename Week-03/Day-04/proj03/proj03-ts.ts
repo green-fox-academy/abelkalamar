f215f45d62174e24b291ffc9244297d910b0f1b7
Abel Kalamar <abel.kalamar@gmail.com>
2018-11-09 10:19:15 +0100

    feat(proj03): remove unneccessary lines

--- Week-03/Day-04/proj03/proj03-ts.ts
+++ Week-03/Day-04/proj03/proj03-ts.ts
@@ -12,6 +12,7 @@ function drawLines(lineDistance: number, color: string): void {
   ctx.beginPath();
   ctx.strokeStyle = color;
   for (let i: number = 0; i < (canvas.width / 2); i += lineDistance) {
+    setTimeout
     ctx.moveTo(i, 0);
     ctx.lineTo(0, y);
     y -= lineDistance;
@@ -20,13 +21,10 @@ function drawLines(lineDistance: number, color: string): void {
 }
 
 function rotateLines(lineDistance: number, color: string):void {
-  ctx.beginPath();
-  ctx.strokeStyle = color;
   for (let i: number = 0; i < 4; i++){
-    ctx.rotate(90 * i/(180/Math.PI));
+    ctx.rotate(90 * - i/(180/Math.PI));
     drawLines(lineDistance, color);
   }
-  ctx.stroke();
 }
 
 rotateLines(10, 'green');
