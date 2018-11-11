48d81c5f357aa40db1bf0421b2239204cda34b69
Abel Kalamar <abel.kalamar@gmail.com>
2018-11-11 13:12:04 +0100

    feat(reverse): add solution with arrow function

--- Week-03/Day-02/02_reverse.ts
+++ Week-03/Day-02/02_reverse.ts
@@ -5,11 +5,13 @@
 
 let reversed: string = '.eslaf eb t\'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI';
 
-
+/*
 function reverse(str: string): string {
   return str.split('').reverse().join('');
 }
+*/
 
+const reverse = (str: string): string => { return str.split('').reverse().join('');}
 console.log(reverse(reversed));
 
 export = reverse;
