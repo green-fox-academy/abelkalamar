4a781ecba6d44f7ab668caa2c228eacdf34cfa1e
Abel Kalamar <abel.kalamar@gmail.com>
2018-11-11 13:30:15 +0100

    feat(matchmaking): rewrite code with arrow function

--- Week-03/Day-02/08_matchmaking.ts
+++ Week-03/Day-02/08_matchmaking.ts
@@ -6,17 +6,16 @@
 let girls: string[] = ['Eve', 'Ashley', 'Claire', 'Kat', 'Jane'];
 let boys: string[] = ['Joe', 'Fred', 'Tom', 'Todd', 'Neef', 'Jeff'];
 
-function makingMatches(arr1: string [], arr2: string[]): string[] {
+const makingMatches = (arr1: string[], arr2: string[]): string[] => {
   let matches: string[] = [];
-  for( let i: number = 0; i < arr2.length; i++){
-    if(arr1[i]){
-    matches.push(girls[i])
+  for (let i: number = 0; i < arr2.length; i++) {
+    if (arr1[i]) {
+      matches.push(girls[i])
     }
     matches.push(boys[i])
   }
   return matches;
 }
-
 console.log(makingMatches(girls, boys));
 
 export = makingMatches;
