4beac7f515837c1650b572f84a8f1fb3a30a3ddf
Abel Kalamar <abel.kalamar@gmail.com>
2018-11-12 22:37:35 +0100

    feat(tic-tac-toe): add function for columns

--- Week-04/Day-01/tic-tac-toe/08_tic-tac-toe.ts
+++ Week-04/Day-01/tic-tac-toe/08_tic-tac-toe.ts
@@ -16,50 +16,59 @@ function readFromFile(fileName: string): string {
   }
 }
 
-function checkRows(fileName: string) {
+function checkRows(fileName: string): string {
   let content: string[] = readFromFile(fileName).split('\r\n');
   let rows: string[][] = [];
-  let result: string = '';
+  let result1: string = '';
   if (content !== null) {
     for (let i: number = 0; i < content.length; i++) {
       rows.push(content[i].split(''));
       if (rows[i].every((e, i, arr) => e === arr[0])) {
-        result = rows[i][0];
+        result1 = rows[i][0];
+      } else {
+        result1 = 'Draw';
       }
     }
   }
-  return result;
+  return result1;
 }
+console.log(checkRows('win-x.txt'));
 
+function checkColumns(fileName: string): string {
+  let content: string[] = readFromFile(fileName).split('\r\n');
+  let columns: any[][] = [];
+  let result2: string = '';
+  if (content !== null) {
+    for (let i: number = 0; i < content.length; i++) {
+      columns.push([]);
+      columns[i].push([]);
+      for (let j: number = 0; j < content[i].length; j++) {
+        columns[i][j] = (content[j][i]);
+      }
+      if (columns[i].every((e, i, arr) => e === arr[0])) {
+        result2 = columns[i][0];
+      } else {
+        result2 = 'Draw';
+      }
+    }
+  }
+  return result2;
+}
+console.log(checkColumns('win-x.txt'));
+
+/*
+function checkDiagonal(fileName: string): string {
+  let content: string[] = readFromFile(fileName).split('\r\n');
+  let result3: string = '';
+  if (content !== null) {
+
+  }
+  return result3;
+}
 console.log(checkRows('win-x.txt'));
 
 
 
-/*
-function result(fileName: string) {
-  let rows: string[] = readFromFile(fileName).split('\r\n');
-  let columns: any [][] = [];
-  let diag: string[][] = [];
-  if (rows !== null) {
-    for (let i: number = 0; i < rows.length; i++) {
-      diag.push(rows[i].split(''));
-    }
-    for (let i: number = 0; i < diag.length; i++) {
-      columns.push([]);
-      columns[i].push([]);
-      for (let j: number = 0; j < diag[i].length; j++) {
-      columns[i][j] = (diag[j][i]);
-      }
-    }
-    
-  }
-  console.log(rows);
-  console.log(diag);
-  console.log(columns);
-}
-
-result('win-x.txt');
-*/
 /*
 console.log(ticTacResult('win-o.txt'))
 // Should print "O"
