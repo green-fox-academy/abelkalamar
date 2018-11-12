b7e1a86d536d0f07b923d5146c9266eca8d9fe92
Abel Kalamar <abel.kalamar@gmail.com>
2018-11-12 17:29:31 +0100

    feat(reverse-lines): add solution

--- Week-04/Day-01/reverse-lines/10_reverse-lines.ts
+++ Week-04/Day-01/reverse-lines/10_reverse-lines.ts
@@ -8,12 +8,17 @@ let fs = require('fs');
 function readFromFile(fileName: string): string {
   try {
     return fs.readFileSync(fileName, 'utf-8');
-  } catch(e) {
+  } catch (e) {
     return null;
   }
 }
 
-function reverseText(fileName: string) {
-  const content = readFromFile(fileName).split('\r\n');
-  
+function reverseText(fileName: string): string {
+  let content: string[] = readFromFile(fileName).split('\r\n');
+  let reversed: string[] = [];
+  content.forEach(e => {
+    reversed.push(e.split('').reverse().join(''));
+  })
+  return reversed.join('\n');
 }
+console.log(reverseText('reversed_lines.txt'));
