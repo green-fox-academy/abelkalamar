e8f8e32d8471a614b61daff8b03da0e22e360922
Abel Kalamar <abel.kalamar@gmail.com>
2018-11-12 14:11:34 +0100

    feat(multilines): add solution

---
+++ Week-04/Day-01/05_write-multiple-lines.ts
@@ -0,0 +1,23 @@
+'use strict';
+export { };
+
+// Create a function that takes 3 parameters: a path, a word and a number,
+// than it should write to a file.
+// The path parameter should be a string, that describes the location of the file.
+// The word parameter should be a string, that will be written to the file as lines
+// The number paramter should describe how many lines the file should have.
+// So if the word is 'apple' and the number is 5, than it should write 5 lines
+// to the file and each line should be 'apple'
+// The function should not raise any error if it could not write the file.
+let fs = require('fs');
+
+function writeIntoFile(fileName: string, data: string): void {
+  fs.appendFileSync(fileName, data);
+}
+
+function writeMultiLines(fileName: string, word: string, lines: number): void {
+  for (let i: number = 0; i < lines; i++) {
+    writeIntoFile(fileName, `${word}\n`);
+  }
+}
+writeMultiLines('multylines.txt', 'fox', 8);
