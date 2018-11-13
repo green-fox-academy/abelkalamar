dbdc98e1b8c67164d5e4aebde68fad5b4647c2d9
Abel Kalamar <abel.kalamar@gmail.com>
2018-11-13 10:33:26 +0100

    feat(01_post-it): add solution

---
+++ Week-04/Day-02/01_post-it.ts
@@ -0,0 +1,16 @@
+class PostIt {
+
+  backgroundColor: string;
+  text: string;
+  textColor: string;
+
+  constructor(bgC: string, txt: string, txtCol: string) {
+    this.backgroundColor = bgC;
+    this.text = txt;
+    this.textColor = txtCol;
+  }
+}
+let obj1 = new PostIt('orange', 'Idea 1', 'blue');
+let obj2 = new PostIt('pink', 'Awesome', 'black');
+let obj3 = new PostIt('yellow', 'Superb', 'green');
+
