2f8be53d78ff10ad929de46a1ea2fb685e192882
Abel Kalamar <abel.kalamar@gmail.com>
2018-11-13 09:29:04 +0100

    feat(tic-tac-toe): 90% working solution

--- Week-04/Day-01/tic-tac-toe/08_tic-tac-toe.ts
+++ Week-04/Day-01/tic-tac-toe/08_tic-tac-toe.ts
@@ -16,7 +16,6 @@ function readFromFile(fileName: string): string {
   }
 }
 
-let result: string = '';
 let result1: string = '';
 let result2: string = '';
 let result3: string = '';
@@ -29,12 +28,13 @@ function checkRows(fileName: string): string {
       rows.push(content[i].split(''));
       if (rows[i].every((e, i, arr) => e === arr[0])) {
         result1 = rows[i][0];
-      } 
+      } else {
+        result1 = 'Draw';
+      }
     }
   }
   return result1;
 }
-//console.log(checkRows('win-x.txt'));
 
 function checkColumns(fileName: string): string {
   let content: string[] = readFromFile(fileName).split('\r\n');
@@ -48,13 +48,15 @@ function checkColumns(fileName: string): string {
       }
       if (columns[i].every((e, i, arr) => e === arr[0])) {
         result2 = columns[i][0];
-      } 
+      } else {
+        result2 = 'Draw';
+      }
     }
   }
+  console.log(columns);
   return result2;
 }
-console.log(checkColumns('win-o.txt'));
-
+checkColumns('win-o.txt')
 
 function checkDiagonal(fileName: string): string {
   let content: any[] = readFromFile(fileName).split('\r\n');
@@ -63,30 +65,39 @@ function checkDiagonal(fileName: string): string {
     for (let i: number = 0; i < content.length; i++) {
       diag.push(content[i].split(''));
     }
-    if (diag[0][0] === diag[1][1] === diag[2][2]) {
+    if (diag[0][0] === diag[1][1] && diag[1][1] === diag[2][2]) {
       result3 = diag[1][1];
-      console.log('cica');
-    } else if (diag[0][2] === diag[1][1] === diag[2][0]) {
+    } else if (diag[0][2] === diag[1][1] && diag[1][1] === diag[2][0]) {
       result3 = diag[1][1];
-      console.log('kutya');
     } else {
       result3 = 'Draw';
-      //console.log('eb');
     }
   }
   return result3;
 }
-//console.log(checkDiagonal('win-x.txt'));
 
 function ticTacResult(fileName: string): string {
-  
+  let result: string = '';
+  if (checkRows(fileName).length === 1) {
+    result = checkRows(fileName);
+  } else if (checkColumns(fileName).length === 1) {
+    console.log('yes');
+    result = checkColumns(fileName);
+  } else if (checkDiagonal(fileName).length === 1) {
+    result = checkDiagonal (fileName);
+  } else {
+    console.log(checkColumns(fileName));
+    result = 'Draw';
+  }
+  return result;
 }
 
-console.log(ticTacResult('win-o.txt'))
+console.log(ticTacResult('win-o.txt'));
 // Should print "O"
-
-console.log(ticTacResult('win-x.txt'))
+/*
+console.log(ticTacResult('win-x.txt'));
 // Should print "X"
 
-console.log(ticTacResult('draw.txt'))
+console.log(ticTacResult('draw.txt'));
 // Should print "Draw"
+*/
