66c37142e6b4c9c9bf5d9a68fc21674c6e2ffe1c
Abel Kalamar <abel.kalamar@gmail.com>
2018-11-15 14:08:55 +0100

    feat(string2): add function

--- Week-04/Day-04/07_strings1.ts
+++ Week-04/Day-04/07_strings1.ts
@@ -1,15 +1,12 @@
 // Given a string, compute recursively (no loops) a new string where all the
 // lowercase 'x' chars have been changed to 'y' chars.
-
 export{ };
 
 function changeXToY(str: string): string {
   if (str.includes('x')) {
     str = str.replace('x', 'y');
-    console.log(str);
     return changeXToY(str);
   }
   return str;
 }
 console.log(changeXToY('xxx'));
-
