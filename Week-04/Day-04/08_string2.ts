eecd2c6bbf1a56f8ae899808bab2f929804b25cd
Abel Kalamar <abel.kalamar@gmail.com>
2018-11-15 14:16:51 +0100

    feat(sring2): add solution

--- Week-04/Day-04/08_string2.ts
+++ Week-04/Day-04/08_string2.ts
@@ -1,5 +1,10 @@
 // Given a string, compute recursively a new string where all the 'x' chars have been removed.
 
-function removeXs(str: string) {
-  
+function changeXToY(str: string): string {
+  if (str.includes('x')) {
+    str = str.replace('x', '');
+    return changeXToY(str);
+  }
+  return str;
 }
+console.log(changeXToY('afsjfdhjxkxxxxskd'));
