7d08bbece39343a125586f45f657abd9cdfc5281
Abel Kalamar <abel.kalamar@gmail.com>
2018-11-17 13:42:35 +0100

    feat(lottery): add lotterynums class, and fill it wiht numbers

--- Week-04/Day-01/lottery/13_lottery.ts
+++ Week-04/Day-01/lottery/13_lottery.ts
@@ -11,21 +11,41 @@ function readFromFile(fileName: string): string {
   }
 }
 
-function getNumbersFromFile(fileName: string): string [][] {
+function getNumbersFromFile(fileName: string): string[][] {
   let content: string[] = readFromFile(fileName).split('\r\n');
   let contentLines: string[][] = [];
   let numsInLines: string[][] = [];
-  content.map(e => {
+  content.forEach(e => {
     contentLines.push(e.split(';'));
   });
-  contentLines.forEach((e, i, a) => {
+  contentLines.forEach(e => {
     numsInLines.push(e.slice(-5));
   });
   return numsInLines;
 }
 
 function frequentNums(arr: string[][]) {
-  
+  class LotteryNums {
+    name: string;
+    frequency: number;
+    
+    constructor(name: string) {
+      this.name = name;
+      this.frequency = 0;
+    }
+  }
+  let nums: LotteryNums[] = [];
+  for (let i = 1; i <= 90; i++) {
+    nums.push(new LotteryNums(i.toString()));
+  }
+  for (let j = 0; j < arr.length; j++) {
+    arr[j].forEach(e => {
+      if(e === )
+      
+    });
+
+  }
+  return nums;
 }
-getNumbersFromFile('lottery.txt')
+console.log(frequentNums(getNumbersFromFile('lottery.txt')));
 
