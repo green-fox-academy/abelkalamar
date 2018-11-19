744add05b8bb3ceefc865ffd014b07b82452137e
Abel Kalamar <abel.kalamar@gmail.com>
2018-11-19 15:46:42 +0100

    feat(aircraft): add F16 and F35 classes

---
+++ Week-05/Day-01/Aircraft-carrier.ts/F16.ts
@@ -0,0 +1,10 @@
+import Aircraft from "./aircraft";
+
+class F16 extends Aircraft {
+
+  constructor() {
+    super(name);
+    this.maxAmmo = 8;
+    this.baseDamage = 30;
+  }
+}
---
+++ Week-05/Day-01/Aircraft-carrier.ts/F35.ts
@@ -0,0 +1,10 @@
+import Aircraft from "./aircraft";
+
+class F35 extends Aircraft {
+
+  constructor() {
+    super(name);
+    this.maxAmmo = 12;
+    this.baseDamage = 50;
+  }
+}
