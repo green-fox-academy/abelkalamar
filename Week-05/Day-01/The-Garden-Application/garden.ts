af1fea5e6e12c4dca199650fe07b55fc998cec91
Abel Kalamar <abel.kalamar@gmail.com>
2018-11-19 14:01:45 +0100

    feat(garden): add watering function

--- Week-05/Day-01/The-Garden-Application/garden.ts
+++ Week-05/Day-01/The-Garden-Application/garden.ts
@@ -9,8 +9,25 @@ class Garden {
   constructor(name: string) {
     this.name = name;
     this.plants = [];
+    console.log(`In ${this.name} garden are ${this.plants.length} plants.`)
   }
 
+  watering(waterAmount: number) {
+    let numOfThirstyPlants = 0;
+    this.plants.forEach(e => {
+      if (e.doesItNeedWater) {
+        numOfThirstyPlants++;
+      }
+    });
+    this.plants.forEach(e => {
+      e.setWaterAmount(waterAmount / numOfThirstyPlants);
+      if (e.doesItNeedWater) {
+      console.log(`The ${e.getColor} ${typeof e} needs water.`);
+      } else {
+        console.log(`The ${e.getColor} ${typeof e} doesn't need water.`)
+      }
+    });
+  }
   
 
 }
