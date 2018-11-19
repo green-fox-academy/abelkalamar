90aa1b08dd3157eb0ee7b622af99f18c8e99f609
Abel Kalamar <abel.kalamar@gmail.com>
2018-11-19 13:36:19 +0100

    feat(garden): add flower class

---
+++ Week-05/Day-01/The-Garden-Application/flower.ts
@@ -0,0 +1,13 @@
+import Plant from './plant';
+
+export default class Flower extends Plant{
+
+  constructor(color: string) {
+    super(color);
+    this.waterAbsorbtion = 0.75;
+  }
+
+  doesItNeedWater() {
+    return super.doesItNeedWater();
+  }
+}
