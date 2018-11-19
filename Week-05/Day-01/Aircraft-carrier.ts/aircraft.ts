4547e9704ca4dc847a5a17d0536b035c5a532270
Abel Kalamar <abel.kalamar@gmail.com>
2018-11-19 15:45:39 +0100

    feat(aircraft): add Aircraft class

---
+++ Week-05/Day-01/Aircraft-carrier.ts/aircraft.ts
@@ -0,0 +1,44 @@
+export default class Aircraft {
+
+  protected name: string;
+  protected maxAmmo: number;
+  protected baseDamage: number;
+  protected currAmmo: number;
+
+  constructor(name: string) {
+    this.name = name;
+    this.currAmmo = 0;
+  }
+
+  fight() {
+    let damage = this.baseDamage * this.currAmmo;
+    this.currAmmo = 0;
+    return damage;
+  }
+
+  refill(ammo: number) {
+    if (ammo > this.currAmmo) {
+      this.currAmmo = this.maxAmmo;
+    } else {
+      this.currAmmo = ammo;
+    }
+    return ammo - this.currAmmo;
+  }
+
+  getType() {
+    return this.constructor.name;
+  }
+
+  getStatus() {
+    console.log(`Type ${this.constructor.name}, Ammo: ${this.currAmmo}, Base Damage: ${this.baseDamage}, All Damage: ${this.currAmmo * this.baseDamage}`);
+  }
+
+  isPriority() {
+    if (this.getType() === 'F16') {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+}
