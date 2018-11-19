cbb8e3e5c1e574d7b61bfee96a335c62dd55cc3f
Abel Kalamar <abel.kalamar@gmail.com>
2018-11-19 17:39:48 +0100

    feat(aircraft): final function invokes

--- Week-05/Day-01/Aircraft-carrier.ts/aircraft-app.ts
+++ Week-05/Day-01/Aircraft-carrier.ts/aircraft-app.ts
@@ -2,8 +2,8 @@ import F35 from './F35';
 import F16 from './F16';
 import Carrier from './carrier';
 
-let gigaMegaCarrier = new Carrier(Math.round(Math.random() * 500), Math.round(Math.random() * 10000));
-let extraBrutalCarrier = new Carrier(Math.round(Math.random() * 500), Math.round(Math.random() * 10000));
+let gigaMegaCarrier = new Carrier('gigaMegaCarrier', Math.round(Math.random() * 500), Math.round(Math.random() * 10000));
+let extraBrutalCarrier = new Carrier('extraBrutalCarrier', Math.round(Math.random() * 500), Math.round(Math.random() * 10000));
 
 gigaMegaCarrier.add(new F16, 5);
 gigaMegaCarrier.add(new F35, 5);
@@ -13,10 +13,7 @@ extraBrutalCarrier.add(new F35, 5);
 gigaMegaCarrier.getStatus();
 extraBrutalCarrier.getStatus();
 
-gigaMegaCarrier.fill();
-extraBrutalCarrier.fill();
+gigaMegaCarrier.fight(extraBrutalCarrier);
 
 gigaMegaCarrier.getStatus();
 extraBrutalCarrier.getStatus();
-
-gigaMegaCarrier.fight(extraBrutalCarrier);
