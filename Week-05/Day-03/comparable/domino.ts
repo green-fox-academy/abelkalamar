826efa3bc69e2c53abc346b077404c3082f4390a
Abel Kalamar <abel.kalamar@gmail.com>
2018-11-21 11:09:01 +0100

    feat(comparable): add interface to domino class

---
+++ Week-05/Day-03/comparable/domino.ts
@@ -0,0 +1,22 @@
+import { Comparable } from "./01_comparable";
+
+ class Domino implements Comparable {
+
+  values: number[];
+
+  constructor(valueA: number, valueB: number) {
+    this.values = [valueA, valueB];
+  }
+
+  compareTo(other: Domino): number {
+    if (this.values[0] < other.values[0]) {
+      return -1;
+    } else if (this.values[0] > other.values[0]) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+}
+
+export { Domino };
