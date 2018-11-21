acab11f2ff1a41010b2f29013f0e17dbd1a2fc55
Abel Kalamar <abel.kalamar@gmail.com>
2018-11-21 12:11:20 +0100

    fix(things): fix style issues

--- Week-05/Day-03/comparable/thing.ts
+++ Week-05/Day-03/comparable/thing.ts
@@ -1,34 +1,34 @@
 import { Comparable } from "./01_comparable_things";
 
 class Thing implements Comparable {
-    private name: string;
-    private completed: boolean;
+  private name: string;
+  private completed: boolean;
 
-    constructor(name: string) {
-        this.name = name;
-        this.completed = false;
-    }
+  constructor(name: string) {
+    this.name = name;
+    this.completed = false;
+  }
 
-    public complete() {
-        this.completed = true;
-    }
+  public complete() {
+    this.completed = true;
+  }
 
-    get getName() {
-        return this.name;
-    }
+  get getName() {
+    return this.name;
+  }
 
-    get getCompleted() {
-        return this.completed;
-    }
+  get getCompleted() {
+    return this.completed;
+  }
 
-    compareTo(other: Thing) {
-        if (this.completed > other.completed) {
-            return -1;
-        } else if (this.completed < other.completed) {
-            return 1;
-        } 
-        return 0;
+  compareTo(other: Thing) {
+    if (this.completed > other.completed) {
+      return -1;
+    } else if (this.completed < other.completed) {
+      return 1;
     }
+    return 0;
+  }
 }
 
 export { Thing };
