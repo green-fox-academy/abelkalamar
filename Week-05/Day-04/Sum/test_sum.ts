bc7a09dece3880a697b5e271deb154029dc26c5e
Abel Kalamar <abel.kalamar@gmail.com>
2018-11-22 11:30:56 +0100

    feat(sum): add test file

---
+++ Week-05/Day-04/Sum/test_sum.ts
@@ -0,0 +1,8 @@
+import * as test from "tape";
+import MyClass from './02_sum';
+
+test('is it equal?', (t: any) => {
+  let nums = [45, 56, 77, 91, 12];
+  let fox = new MyClass;
+  t.equal(fox.sum(), )
+})
