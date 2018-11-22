46555ff613c6e40d34f0d2fba932168fb176c2d5
Abel Kalamar <abel.kalamar@gmail.com>
2018-11-22 14:49:33 +0100

    fix(fibonacci): style issues

--- Week-05/Day-04/Fibonacci/05_fibonacci.ts
+++ Week-05/Day-04/Fibonacci/05_fibonacci.ts
@@ -1,10 +1,13 @@
 export default function fibonacci(num: number) {
+
   if (num === 0) {
     return 0;
   } else if (num === 1) {
     return 1;
+  } else if (num < 0) {
+    throw 'Num is less than 0!';
+  } else if (num % 1 !== 0) {
+    throw 'Num must be an integer!';
   }
   return fibonacci(num - 1) + fibonacci(num - 2);
 }
-
-console.log(fibonacci(10));
