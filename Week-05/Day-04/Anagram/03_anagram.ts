49cd9122e4d7861a25d7a7e9fd59c2cf403c8e30
Abel Kalamar <abel.kalamar@gmail.com>
2018-11-22 11:54:31 +0100

    feat(anagram): add anagram function

---
+++ Week-05/Day-04/Anagram/03_anagram.ts
@@ -0,0 +1,6 @@
+// Write a function, that takes two strings and returns a boolean value based on if the two strings are Anagramms or not.
+// Create a test for that.
+
+export default function areAnagrams(a: string, b: string): boolean {
+  return a.toLowerCase().split('').sort().join('') === b.toLowerCase().split('').sort().join('');
+}
