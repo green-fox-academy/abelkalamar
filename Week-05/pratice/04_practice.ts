9e92974dc309676ee47e73aac22a75b81eee1113
Abel Kalamar <abel.kalamar@gmail.com>
2018-11-25 23:03:18 +0100

    feat(practice for-in, and of): add practice file

--- Week-05/pratice/04_practice.ts
+++ Week-05/pratice/04_practice.ts
@@ -1,17 +1,33 @@
 import fs from 'fs';
 
 let content = fs.readFileSync('objectContent.txt', 'utf8').trim().split('\r\n');
-let contentLines = content.map(e => {return e.split('  ')});
-let contentObject = contentLines.reduce((elems, lines): Object => {
+let contentLines = content.map(e => { return e.split('  ') });
+let climbers = contentLines.reduce((elems, lines): Object => {
   elems[lines[0]] = elems[lines[0]] || []
   elems[lines[0]].push({
-    type : lines[1],
-    grade : lines[2]
+    type: lines[1],
+    grade: lines[2]
   })
   return elems;
 }, {})
 
-console.log(contentObject);
+// console.log(climbers);
 
 // make a list of all of the grades 
 
+let nameOfClimbers: string[] = [];
+let climbingTypes: string[] = [];
+let climbingGrades: string[] = [];
+for (let i in climbers) {
+  nameOfClimbers.push(i);
+  climbingTypes.push(climbers[i][0].type);
+  climbingGrades.push(climbers[i][0].grade);
+}
+
+console.log(nameOfClimbers);
+console.log(climbingTypes);
+console.log(climbingGrades);
+
+for (let i of climbingGrades) {
+  console.log(i);
+}
