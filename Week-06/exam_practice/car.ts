8ada30e1dfa829a86b5d11f5ed22c943274f7501
Abel Kalamar <abel.kalamar@gmail.com>
2018-11-29 12:05:32 +0100

    feat(carPark): add hasTicket parameter, and getHasTicket method

--- Week-06/exam_practice/car.ts
+++ Week-06/exam_practice/car.ts
@@ -1,26 +1,30 @@
-import FileIo from './file-io';
-
-export class Car {
+export default class Car {
 
     private id: number;
     private licensePlate: string;
     private manufactureYear: number;
+    private hasTicket: number;
 
-    constructor(id: number, licensePlate: string, manufactureYear: number) {
+    constructor(id: number, licensePlate: string, manufactureYear: number, hasTicket: number) {
       this.id = id;
       this. licensePlate = licensePlate;
       this.manufactureYear = manufactureYear;
+      this.hasTicket = hasTicket;
     }
 
-    getId() {
+    getId(): number {
       return this.id;
     }
 
-    getLicensePlate() {
+    getLicensePlate(): string {
       return this. licensePlate;
     }
 
-    getManufactureYear() {
+    getManufactureYear(): number {
       return this.manufactureYear;
+    }
+
+    getHasTicket(): number {
+      return this.hasTicket;
     }
 }
