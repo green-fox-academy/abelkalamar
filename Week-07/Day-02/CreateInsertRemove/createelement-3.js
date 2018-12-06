b4964ad99ff48020bce1189fc91c2a2280de1513
Abel Kalamar <abel.kalamar@gmail.com>
2018-12-06 16:40:53 +0100

    feat(createRemoveInsert03): add html and js files for 3.rd task

---
+++ Week-07/Day-02/CreateInsertRemove/createelement-3.js
@@ -0,0 +1,34 @@
+'use strict'
+
+// 1) Create an <article> element for each kid
+// 2) Create a <h3> and a <p> element for each article and append them as a child to the <article>
+//     - The H3 should contain the owner's name
+//     - The p should contain the pet's name
+// 3) Add the article to the pets div.
+
+const kids = [
+  { 'petName': 'Wattled crane', 'owner': 'Bryant' },
+  { 'petName': 'Devil, tasmanian', 'owner': 'Alejandro' },
+  { 'petName': 'Mynah, common', 'owner': 'Nelie' },
+  { 'petName': 'Dolphin, common', 'owner': 'Mariele' },
+  { 'petName': 'Gray duiker', 'owner': 'Maddalena' },
+  { 'petName': 'Crab (unidentified)', 'owner': 'Lucine' },
+  { 'petName': 'Ant (unidentified)', 'owner': 'Lorianna' },
+  { 'petName': 'Bison, american', 'owner': 'Tommie' },
+  { 'petName': 'Yellow mongoose', 'owner': 'Vivyan' },
+  { 'petName': 'Carpet snake', 'owner': 'Veda' },
+  { 'petName': 'Lesser mouse lemur', 'owner': 'Isidor' },
+];
+
+const pets = document.querySelector('div#pets');
+
+kids.forEach(e => {
+  const kid = document.createElement('article');
+  const owner = document.createElement('h3');
+  owner.textContent = e.owner;
+  kid.appendChild(owner);
+  const paragraph = document.createElement('p');
+  paragraph.textContent = e.petName;
+  kid.appendChild(paragraph);
+  pets.appendChild(kid);
+})
