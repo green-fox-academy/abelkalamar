3873ac9bc193088aaabfa18c532a28c5041aea10
Abel Kalamar <abel.kalamar@gmail.com>
2018-12-06 12:18:10 +0100

    feat(AnElementsContent2): add html and js files

---
+++ Week-07/Day-02/AnElementsContent/content-2.js
@@ -0,0 +1,14 @@
+// 1) replace the list items' content with items from this list:
+//['apple', 'banana', 'cat', 'dog']
+//2) change the <ul> element's background color to 'limegreen'
+//- use css class to change the color instead of the style property
+
+const list = document.querySelectorAll('li');
+// console.log(list);
+
+const contents = ['apple', 'banana', 'cat', 'dog'];
+
+for (let i = 0; i < list.length; i++) {
+  list[i].innerText = contents[i];
+  console.log(list[i].innerText);
+}
