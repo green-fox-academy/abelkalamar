65d5552c7da83b137ab8a5ba39d4f452376f66aa
Abel Kalamar <abel.kalamar@gmail.com>
2018-12-06 14:39:02 +0100

    feat(createRemoveInsert): add 3. and 4. task solution to js file

--- Week-07/Day-02/CreateInsertRemove/createelement-1.js
+++ Week-07/Day-02/CreateInsertRemove/createelement-1.js
@@ -4,6 +4,7 @@
 // Add an image, any image, to the container.
 
 const list = document.querySelector('.asteroids');
+const container = document.querySelector('.container');
 const greenFox = document.createElement('li');
 greenFox.innerText = 'The Green Fox';
 list.appendChild(greenFox);
@@ -12,3 +13,10 @@ const lampLighter = document.createElement('li');
 lampLighter.innerText = 'The Lamplighter';
 list.appendChild(lampLighter);
 
+const header = document.createElement('header');
+header.innerText = 'I can add elements to the DOM!';
+container.appendChild(header);
+
+const image = document.createElement('img');
+image.setAttribute('src', 'https://www.thoughtco.com/thmb/_wj4kFqJQiKnSn4naqyRaIzRZNM=/423x487/filters:no_upscale()/christmastree6-56a26c9f5f9b58b7d0ca1cdc.jpg');
+container.appendChild(image);
