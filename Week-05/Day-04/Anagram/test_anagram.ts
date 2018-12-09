2edbbc171da70c90c1db686599eebfa6fc8eb73a
Abel Kalamar <abel.kalamar@gmail.com>
2018-12-09 10:20:12 +0100

    feat(anagram.task): fix style issues

--- Week-05/Day-04/Anagram/test_anagram.ts
+++
@@ -1,7 +0,0 @@
-import { test } from 'tape';
-import areAnagrams from './03_anagram';
-
-test('are these words anagrams?', (t: any) => {
-  t.equal(areAnagrams('need', 'eedn'), true);
-  t.end();
-})
