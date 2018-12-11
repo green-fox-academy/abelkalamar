4153d706eb7e5b558ae09c467dcad1111517993c
Abel Kalamar <abel.kalamar@gmail.com>
2018-12-11 17:46:30 +0100

    feat(star-wars-api): add function for read value from input box, and give back the character object

--- Week-08/Day-02/star-wars-api/star-wars-api.js
+++ Week-08/Day-02/star-wars-api/star-wars-api.js
@@ -1,9 +1,28 @@
 const xhr = new XMLHttpRequest();
-const peopleUrl = 'https://swapi.co/api/people';
+const url = 'https://swapi.co/api/people/';
+const main = document.querySelector('#main');
+
+xhr.onload = () => {
+  if (xhr.status === 200) {
+    const content = JSON.parse(xhr.responseText);
+    console.log(content);
+    // content.results.forEach((e, i) => {
+    //   const char = document.createElement('button');
+    //   character.innerHTML = content.results[i].name;
+    //   main.appendChild(char);
+    // })
+  }
+}
+
+const button = document.querySelector('button');
+
+button.onclick = () => {
+  const inputValue = document.querySelector('#full-name');
+  let name = inputValue.value;
+  // console.log(name);
+  xhr.open('GET', `${url}?search=${name}`);
+  xhr.send();
+}
 
 
 
-xhr.open('GET', url);
-xhr.send();
-
-console.log()
