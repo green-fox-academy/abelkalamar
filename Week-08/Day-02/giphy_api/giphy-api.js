2faa0f5ba0b39f0deb5d7d7bbc37d957895d69f6
Abel Kalamar <abel.kalamar@gmail.com>
2018-12-11 14:19:10 +0100

    feat(giphyAPI): add still images of 16 cat giphs to my page

---
+++ Week-08/Day-02/giphy_api/giphy-api.js
@@ -0,0 +1,22 @@
+
+const getGiphies = () => {
+  const xhr = new XMLHttpRequest();
+  const URL = 'https://api.giphy.com/v1/gifs/search?api_key=PYXQGIvR8i9QDq23exxMSJShmkLRmvbP&q=cat&limit=16&offset=0&rating=G&lang=en';
+  const content = document.querySelector('.container');
+
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      const result = JSON.parse(xhr.responseText);
+      console.log(result);
+      for (let i = 0; i < result.data.length; i++) {
+        let img = document.createElement('img');
+        img.setAttribute('src', result.data[i].images.original_still.url);
+        content.appendChild(img);
+      }
+    }
+  }
+  xhr.open('GET', URL);
+  xhr.send();
+}
+
+getGiphies();
