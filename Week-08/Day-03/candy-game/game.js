7d8a1b0a7af217d4b068b3c637d1cd4608babde7
Abel Kalamar <abel.kalamar@gmail.com>
2018-12-12 15:08:14 +0100

    feat(candyGame): make lollipopp button to work, and write createSpeed to the screen

--- Week-08/Day-03/candy-game/game.js
+++ Week-08/Day-03/candy-game/game.js
@@ -1,8 +1,34 @@
 const createCandies = document.querySelector('.create-candies');
 const candieCount = document.querySelector('.candies');
+const candieCreateRate = document.querySelector('.speed');
+const buyLollipop = document.querySelector('.buy-lollypops');
+const lollipopCount = document.querySelector('.lollypops');
+
 let myCandies = 0;
+let myLollipops = '';
+let myLollipopCounter = 0;
+let candiesPerSec = 0;
 
 createCandies.addEventListener('click', () => {
-  myCandies++;
+  myCandies += 100;
   candieCount.textContent = myCandies;
 })
+
+buyLollipop.addEventListener('click', () => {
+  if (myCandies >= 100) {
+    myLollipops += '🍭';
+    myLollipopCounter++;
+    lollipopCount.textContent = myLollipops;
+    myCandies -= 100;
+    candieCount.textContent = myCandies;
+    if (myLollipopCounter >= 10) {
+      candiesPerSec++;
+      myLollipopCounter = 0;
+    }
+    candieCreateRate.textContent = candiesPerSec;
+
+  }
+})
+
+
+
