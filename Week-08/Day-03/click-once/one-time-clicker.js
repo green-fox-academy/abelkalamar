98511f6278885599f9397ead57c76aaca12ae8a4
Abel Kalamar <abel.kalamar@gmail.com>
2018-12-12 11:40:08 +0100

    feat(one-time-click): add html and js files for 1st solution

---
+++ Week-08/Day-03/click-once/one-time-clicker.js
@@ -0,0 +1,15 @@
+// Create an event listener which logs the current timestamp to the console once the button is clicked.
+// When the button is clicked for the second or any other times no event listener should be called.
+// Try to solve this two different ways.
+
+// 1st solution
+
+const button = document.querySelector('button');
+const logTimeStamp = (event) => {
+  console.log(`The button pressed after ${event.timeStamp / 1000} seconds the page loaded.`);
+  button.removeEventListener('click', logTimeStamp);
+}
+button.addEventListener('click', logTimeStamp);
+
+// 2nd solution
+
