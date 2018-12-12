adf52164088666b7eb4117ccf88c29c7af1b06b4
Abel Kalamar <abel.kalamar@gmail.com>
2018-12-12 15:59:59 +0100

    feat(candyGame): add function for Candy Rain button

--- Week-08/Day-03/candy-game/game.js
+++ Week-08/Day-03/candy-game/game.js
@@ -3,16 +3,30 @@ const candieCount = document.querySelector('.candies');
 const candieCreateRate = document.querySelector('.speed');
 const buyLollipop = document.querySelector('.buy-lollypops');
 const lollipopCount = document.querySelector('.lollypops');
+const candyRain = document.querySelector('.candy-machine');
 
 let myCandies = 0;
 let myLollipops = '';
 let myLollipopCounter = 0;
 let candiesPerSec = 0;
 
-createCandies.addEventListener('click', () => {
-  myCandies += 100;
+
+const addCandies = () => {
+  myCandies++;
   candieCount.textContent = myCandies;
-})
+}
+
+const periodCandies = () => {
+  myCandies += candiesPerSec;
+  candieCount.textContent = myCandies;
+}
+
+const candyMachine = () => {
+  candiesPerSec = candiesPerSec * 10;
+  candieCreateRate.textContent = candiesPerSec;
+}
+
+createCandies.addEventListener('click', addCandies);
 
 buyLollipop.addEventListener('click', () => {
   if (myCandies >= 100) {
@@ -21,14 +35,26 @@ buyLollipop.addEventListener('click', () => {
     lollipopCount.textContent = myLollipops;
     myCandies -= 100;
     candieCount.textContent = myCandies;
-    if (myLollipopCounter >= 10) {
-      candiesPerSec++;
-      myLollipopCounter = 0;
-    }
-    candieCreateRate.textContent = candiesPerSec;
-
   }
 })
 
+buyLollipop.addEventListener('click', () => {
+  if (myLollipopCounter >= 10) {
+    candiesPerSec++;
+    myLollipopCounter = 0;
+  }
+  candieCreateRate.textContent = candiesPerSec;
+})
 
+// buyLollipop.addEventListener('click', () => {
+//   if (myLollipopCounter === 9) {
+//     clearInterval(interval);
+//   }
+//   if (myLollipopCounter === 10) {
+//     let interval = setInterval(periodCandies, 1000);
+//   }
+// })
 
+candyRain.addEventListener('click', candyMachine);
+
+myCandies = 2000;
