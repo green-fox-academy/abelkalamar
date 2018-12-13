5d08102812ee5c1c07b6d3ad63fc51508ee965f2
Abel Kalamar <abel.kalamar@gmail.com>
2018-12-13 17:41:10 +0100

    feat(movie-selector): need to work on the files, hardly working

--- Week-08/Day-04/movie-selector/movie-selector.js
+++ Week-08/Day-04/movie-selector/movie-selector.js
@@ -1,6 +1,6 @@
 const genre = document.querySelector('.form-list select');
 const movies = document.querySelector('#movies');
-console.log(movies);
+
 
 const dramas = ['Select a movie', '21 grams', 'Seven', 'Babel'];
 const scifis = ['Select a movie', 'Prometheus', 'Life', 'Ex Machina'];
@@ -8,7 +8,14 @@ const actions = ['Select a movie', 'Avengers', 'Sicario', 'John Wick'];
 
 const movieForm = document.querySelector('.lists');
 
+const clearer = () => {
+  while (movies.firstChild) {
+    
+  } 
+}
+
 const addMovies = () => {
+  movies.value = '';
   switch (genre.value) {
     case 'sci-fi':
       scifis.forEach(e => {
@@ -34,4 +41,10 @@ const addMovies = () => {
   }
 }
 
+const printTitle = () => {
+  const result = document.querySelector('.resultMovie');
+  result.textContent = movies.value;
+}
+
 movieForm.addEventListener('change', addMovies);
+movies.addEventListener('change', printTitle);
