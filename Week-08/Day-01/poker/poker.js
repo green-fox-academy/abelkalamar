d64311dd1f587ee345d3cca43fd0e9b7fa491d46
Abel Kalamar <abel.kalamar@gmail.com>
2018-12-14 09:15:20 +0100

    feat(pokerGame): add cardDeck object

---
+++ Week-08/Day-01/poker/poker.js
@@ -0,0 +1,262 @@
+const cardDeck = [
+  {
+    'name': '2D',
+    'value': 1,
+    'img': './img/2D.jpg'
+  },
+  {
+    'name': '3D',
+    'value': 2,
+    'img': './img/3D.jpg'
+  },
+  {
+    'name': '4D',
+    'value': 3,
+    'img': './img/4D.jpg'
+  },
+  {
+    'name': '5D',
+    'value': 4,
+    'img': './img/5D.jpg'
+  },
+  {
+    'name': '6D',
+    'value': 5,
+    'img': './img/6D.jpg'
+  },
+  {
+    'name': '7D',
+    'value': 6,
+    'img': './img/7D.jpg'
+  },
+  {
+    'name': '8D',
+    'value': 7,
+    'img': './img/8D.jpg'
+  },
+  {
+    'name': '9D',
+    'value': 8,
+    'img': './img/9D.jpg'
+  },
+  {
+    'name': 'TD',
+    'value': 9,
+    'img': './img/TD.jpg'
+  },
+  {
+    'name': 'JD',
+    'value': 10,
+    'img': './img/JD.jpg'
+  },
+  {
+    'name': 'QD',
+    'value': 11,
+    'img': './img/QD.jpg'
+  },
+  {
+    'name': 'KD',
+    'value': 12,
+    'img': './img/KD.jpg'
+  },
+  {
+    'name': 'AD',
+    'value': 13,
+    'img': './img/AD.jpg'
+  },
+  {
+    'name': '2C',
+    'value': 1,
+    'img': './img/2C.jpg'
+  },
+  {
+    'name': '3C',
+    'value': 2,
+    'img': './img/3C.jpg'
+  },
+  {
+    'name': '4C',
+    'value': 3,
+    'img': './img/4C.jpg'
+  },
+  {
+    'name': '5C',
+    'value': 4,
+    'img': './img/5C.jpg'
+  },
+  {
+    'name': '6C',
+    'value': 5,
+    'img': './img/6C.jpg'
+  },
+  {
+    'name': '7C',
+    'value': 6,
+    'img': './img/7C.jpg'
+  },
+  {
+    'name': '8C',
+    'value': 7,
+    'img': './img/8C.jpg'
+  },
+  {
+    'name': '9C',
+    'value': 8,
+    'img': './img/9C.jpg'
+  },
+  {
+    'name': 'TC',
+    'value': 9,
+    'img': './img/TC.jpg'
+  },
+  {
+    'name': 'JC',
+    'value': 10,
+    'img': './img/JC.jpg'
+  },
+  {
+    'name': 'QC',
+    'value': 11,
+    'img': './img/QC.jpg'
+  },
+  {
+    'name': 'KC',
+    'value': 12,
+    'img': './img/KC.jpg'
+  },
+  {
+    'name': 'AC',
+    'value': 13,
+    'img': './img/AC.jpg'
+  },
+  {
+    'name': '2H',
+    'value': 1,
+    'img': './img/2H.jpg'
+  },
+  {
+    'name': '3H',
+    'value': 2,
+    'img': './img/3H.jpg'
+  },
+  {
+    'name': '4H',
+    'value': 3,
+    'img': './img/4H.jpg'
+  },
+  {
+    'name': '5H',
+    'value': 4,
+    'img': './img/5H.jpg'
+  },
+  {
+    'name': '6H',
+    'value': 5,
+    'img': './img/6H.jpg'
+  },
+  {
+    'name': '7H',
+    'value': 6,
+    'img': './img/7H.jpg'
+  },
+  {
+    'name': '8H',
+    'value': 7,
+    'img': './img/8H.jpg'
+  },
+  {
+    'name': '9H',
+    'value': 8,
+    'img': './img/9H.jpg'
+  },
+  {
+    'name': 'TH',
+    'value': 9,
+    'img': './img/TH.jpg'
+  },
+  {
+    'name': 'JH',
+    'value': 10,
+    'img': './img/JH.jpg'
+  },
+  {
+    'name': 'QH',
+    'value': 11,
+    'img': './img/QH.jpg'
+  },
+  {
+    'name': 'KH',
+    'value': 12,
+    'img': './img/KH.jpg'
+  },
+  {
+    'name': 'AH',
+    'value': 13,
+    'img': './img/AH.jpg'
+  },
+  {
+    'name': '2S',
+    'value': 1,
+    'img': './img/2S.jpg'
+  },
+  {
+    'name': '3S',
+    'value': 2,
+    'img': './img/3S.jpg'
+  },
+  {
+    'name': '4S',
+    'value': 3,
+    'img': './img/4S.jpg'
+  },
+  {
+    'name': '5S',
+    'value': 4,
+    'img': './img/5S.jpg'
+  },
+  {
+    'name': '6S',
+    'value': 5,
+    'img': './img/6S.jpg'
+  },
+  {
+    'name': '7S',
+    'value': 6,
+    'img': './img/7S.jpg'
+  },
+  {
+    'name': '8S',
+    'value': 7,
+    'img': './img/8S.jpg'
+  },
+  {
+    'name': '9S',
+    'value': 8,
+    'img': './img/9S.jpg'
+  },
+  {
+    'name': 'TS',
+    'value': 9,
+    'img': './img/TS.jpg'
+  },
+  {
+    'name': 'JS',
+    'value': 10,
+    'img': './img/JS.jpg'
+  },
+  {
+    'name': 'QS',
+    'value': 11,
+    'img': './img/QS.jpg'
+  },
+  {
+    'name': 'KS',
+    'value': 12,
+    'img': './img/KS.jpg'
+  },
+  {
+    'name': 'AS',
+    'value': 13,
+    'img': './img/AS.jpg'
+  }
+]
