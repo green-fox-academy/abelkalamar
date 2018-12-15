0e87f79966fad5d8d1c1e41b8d34775e408191c0
Abel Kalamar <abel.kalamar@gmail.com>
2018-12-15 17:42:04 +0100

    feat(starWarsAPI): show names what we search on

--- Week-08/Day-02/star-wars-api/star-wars-api.js
+++ Week-08/Day-02/star-wars-api/star-wars-api.js
@@ -1,11 +1,21 @@
 const xhr = new XMLHttpRequest();
 const url = 'https://swapi.co/api/people/';
-const main = document.querySelector('#main');
+const main = document.querySelector('.main');
+
+const clearer = () => {
+}
 
 xhr.onload = () => {
   if (xhr.status === 200) {
     const content = JSON.parse(xhr.responseText);
-    console.log(content);
+    console.log(content.results);
+    content.results.forEach(e => {
+      let charName = document.createElement('p');
+      charName.textContent = e.name;
+      console.log(charName);
+      
+      main.appendChild(charName);
+    });
     // content.results.forEach((e, i) => {
     //   const char = document.createElement('button');
     //   character.innerHTML = content.results[i].name;
@@ -19,7 +29,6 @@ const button = document.querySelector('button');
 button.onclick = () => {
   const inputValue = document.querySelector('#full-name');
   let name = inputValue.value;
-  // console.log(name);
   xhr.open('GET', `${url}?search=${name}`);
   xhr.send();
 }
