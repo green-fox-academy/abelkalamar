b56a0a8c2cbf09c13931d28ca4e2bc8c022954aa
Abel Kalamar <abel.kalamar@gmail.com>
2018-12-23 21:57:31 +0100

    refact(star-wars-API): refactor the code from almost zero, with smaller functions, add char-name print function

--- Week-08/Day-02/star-wars-api/star-wars-api.js
+++ Week-08/Day-02/star-wars-api/star-wars-api.js
@@ -1,36 +1,64 @@
 const xhr = new XMLHttpRequest();
 const url = 'https://swapi.co/api/people/';
-const main = document.querySelector('.main');
+const characters = document.querySelector('.charList');
+const button = document.querySelector('button');
 
 const clearer = () => {
-}
-
-xhr.onload = () => {
-  if (xhr.status === 200) {
-    const content = JSON.parse(xhr.responseText);
-    console.log(content.results);
-    content.results.forEach(e => {
-      let charName = document.createElement('p');
-      charName.textContent = e.name;
-      console.log(charName);
-      
-      main.appendChild(charName);
-    });
-    // content.results.forEach((e, i) => {
-    //   const char = document.createElement('button');
-    //   character.innerHTML = content.results[i].name;
-    //   main.appendChild(char);
-    // })
+  while (characters.firstChild) {
+    characters.removeChild(characters.firstChild);
   }
 }
 
-const button = document.querySelector('button');
+const selectChar = () => {
+  characters.addEventListener('click', (event) => {
+    console.log(event.target);
+  })
+}
+
+const printChar = (list) => {
+  list.forEach(e => {
+    let charName = document.createElement('p');
+    charName.textContent = e.name;
+    characters.appendChild(charName);
+  });
+}
+
+const turnPage = (nextPage) => {
+  while (nextPage) {
+    console.log('ok');
+    const newXHR = new XMLHttpRequest();
+    newXHR.onload = () => {
+      if (newXHR.status === 200) {
+        const nextContent = JSON.parse(newXHR.responseText);
+        printChar(nextContent.results);
+        newXHR.open('GET', `${content.next}`);
+        newXHR.send();
+      }
+    }
+  }
+}
+
+const sendHttpRequest = (method, url, callback) => {
+  const xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      callback(JSON.parse(xhr.responseText));
+    }
+  }
+  xhr.send();
+}
 
 button.onclick = () => {
   const inputValue = document.querySelector('#full-name');
   let name = inputValue.value;
-  xhr.open('GET', `${url}?search=${name}`);
-  xhr.send();
+  sendHttpRequest('GET', `${url}?search=${name}`, (response) => {
+    const content = response;
+    console.log(content);
+    clearer();
+    printChar(content.results);
+    selectChar();
+  })
 }
 
 
