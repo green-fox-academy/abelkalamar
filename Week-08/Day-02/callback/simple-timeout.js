a1b25485a9162382c27457d4f0d875283a842b2c
Abel Kalamar <abel.kalamar@gmail.com>
2018-12-23 23:22:16 +0100

    feat(simple-timeout): relocate some files, and add solution for simple timeout task

---
+++ Week-08/Day-02/callback/simple-timeout.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Write a program that prints apple after 3 seconds.
+
+const applePrinter = () => {
+  setTimeout(() => {
+    console.log('apple');
+  }, 3000);
+}
+applePrinter();
