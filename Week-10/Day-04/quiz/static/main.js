49329148ccc9a7858e538184a00059931827f359
Abel Kalamar <abel.kalamar@gmail.com>
2019-01-07 22:09:00 +0100

    feat(quiz-app): score increasing in case of good answer

--- Week-10/Day-04/quiz/static/main.js
+++ Week-10/Day-04/quiz/static/main.js
@@ -1,6 +1,5 @@
 'use strict'
 
-const xhr = new XMLHttpRequest();
 
 const question = document.querySelector('.question');
 const firstAnswer = document.querySelector('.answer_one');
@@ -8,10 +7,21 @@ const secondAnswer = document.querySelector('.answer_two');
 const thirdAnswer = document.querySelector('.answer_three');
 const fourthAnswer = document.querySelector('.answer_four');
 const answersDiv = document.querySelector('.content');
+const scoreSpan = document.querySelector('.score');
 
-xhr.open('GET', '/game');
-xhr.onload = (response) => {
-  const questions = JSON.parse(xhr.responseText);
+const sendHTTPRequest = (method, url, callback) => {
+  const xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.send();
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      callback(JSON.parse(xhr.responseText));
+    }
+  }
+}
+
+sendHTTPRequest('GET', '/game', (response) => {
+  const questions = response;
   const id = chooseId(questions) * 4;
   console.log(questions);
   question.textContent = questions[id].question;
@@ -23,17 +33,19 @@ xhr.onload = (response) => {
   thirdAnswer.setAttribute('data', questions[id + 2].is_correct);
   fourthAnswer.textContent = questions[id + 3].answer;
   fourthAnswer.setAttribute('data', questions[id + 3].is_correct);
-};
-xhr.send();
+});
 
-const chooseId = (list) =>{
+const chooseId = (list) => {
   return Math.floor(Math.random() * (list.length / 4));
 };
 
 answersDiv.addEventListener('click', (event) => {
   if (event.target.getAttribute('data') == 1) {
     event.target.setAttribute('style', 'background: #27AE60');
-  } else {
+    let currentScore = +(scoreSpan.textContent);
+    currentScore++;
+    scoreSpan.textContent = currentScore;
+  } else if (event.target.getAttribute('data') == 0) {
     event.target.setAttribute('style', 'background: #EB6654');
   }
 });
