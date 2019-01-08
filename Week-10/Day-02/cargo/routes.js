3240a47a42b4049775570d7c388302e4304fc416
Abel Kalamar <abel.kalamar@gmail.com>
2019-01-08 13:41:07 +0100

    feat(cargo): add second endpoint

--- Week-10/Day-02/cargo/routes.js
+++ Week-10/Day-02/cargo/routes.js
@@ -15,4 +15,20 @@ app.get('/rocket', (req, res) => {
   });
 });
 
+app.get('/rocket/fill', (req, res) => {
+  const { caliber, amount } = req.query;
+  if (!caliber || !amount) {
+    res.status(220).json({
+      error: 'Missing queries'
+    });
+  } else if (caliber && amount) {
+    res.json({
+      received: caliber,
+      amount: +amount,
+      shipstatus: amount / 12500 * 100 + '%',
+      ready: amount >= 12500
+    });
+  }
+});
+
 module.exports = app;
