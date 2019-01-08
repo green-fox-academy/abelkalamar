cbb30fd040b70658874f96b59a4136022bf66a06
Abel Kalamar <abel.kalamar@gmail.com>
2019-01-08 16:28:02 +0100

    feat(cargo): change things

--- Week-10/Day-02/cargo/routes.js
+++ Week-10/Day-02/cargo/routes.js
@@ -5,13 +5,19 @@ const app = express();
 
 app.use(express.json());
 
+let caliber25 = 0;
+let caliber30 = 0;
+let caliber50 = 0;
+let totalAmount = 0;
+console.log(totalAmount);
+
 app.get('/rocket', (req, res) => {
   res.json({
-    caliber25: 0,
-    caliber30: 0,
-    caliber50: 0,
-    shipstatus: 'empty',
-    ready: false
+    caliber25: +caliber25,
+    caliber30: +caliber30,
+    caliber50: +caliber50,
+    shipstatus: shipStatus(totalAmount),
+    ready: +totalAmount === 12500
   });
 });
 
@@ -22,13 +28,30 @@ app.get('/rocket/fill', (req, res) => {
       error: 'Missing queries'
     });
   } else if (caliber && amount) {
+    if (caliber == .25) {
+      caliber25 += amount;
+    } else if (caliber == .30) {
+      caliber30 += amount;
+    } else if (caliber == .50) {
+      caliber50 += amount;
+    }
+    totalAmount = +caliber25 + +caliber30 + +caliber50;
     res.json({
       received: caliber,
       amount: +amount,
-      shipstatus: amount / 12500 * 100 + '%',
-      ready: amount >= 12500
+      shipstatus: shipStatus(totalAmount),
+      ready: +totalAmount === 12500,
+      totalAmount: totalAmount
     });
   }
 });
+
+const shipStatus = (amount) => {
+  if (amount <= 12500) {
+    return amount / 12500 * 100 + '%';
+  } else {
+    return 'overloaded';
+  }
+}
 
 module.exports = app;
--- Week-10/Day-02/cargo/test/test.js
+++ Week-10/Day-02/cargo/test/test.js
@@ -7,4 +7,17 @@ const request = require('supertest');
 test('/rocket endpoint', (t) => {
   request(app)
     .get('/rocket')
-})
+    .expect(200)
+    .end((err, res) => {
+      if (err) throw (err);
+      const expectedRes = {
+        caliber25: 0,
+        caliber30: 0,
+        caliber50: 0,
+        shipstatus: '0%',
+        ready: false
+      }
+      t.same(res.body, expectedRes);
+      t.end();
+    });
+});
