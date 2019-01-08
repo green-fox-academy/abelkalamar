7eb3557f37ae80a984b31c1a5e718f566c6e5fdd
Abel Kalamar <abel.kalamar@gmail.com>
2019-01-08 12:27:33 +0100

    feat(cargo): create endpoint backend

--- Week-10/Day-02/cargo/routes.js
+++ Week-10/Day-02/cargo/routes.js
@@ -6,7 +6,13 @@ const app = express();
 app.use(express.json());
 
 app.get('/rocket', (req, res) => {
-  
-})
+  res.json({
+    caliber25: 0,
+    caliber30: 0,
+    caliber50: 0,
+    shipstatus: 'empty',
+    ready: false
+  });
+});
 
 module.exports = app;
