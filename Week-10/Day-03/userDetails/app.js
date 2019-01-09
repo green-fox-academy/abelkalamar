c5cce1c7c81aeb81e2dc5e6d793f443f5f19c4ac
Abel Kalamar <abel.kalamar@gmail.com>
2019-01-09 15:59:35 +0100

    feat(userDetails): add first endpoint for checking the login data

--- Week-10/Day-03/userDetails/app.js
+++ Week-10/Day-03/userDetails/app.js
@@ -25,6 +25,44 @@ app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname + '/login.html'));
 });
 
+app.get('/user', (req, res) => {
+  const { username, password } = req.query;
+  if (username && password) {
+    const sqlSearch = 'SELECT * FROM userContent'
+    conn.query(sqlSearch, (err, data) => {
+      if (err) {
+        res.status(500).json({
+          error: 'Internal server error'
+        });
+        return;
+      }
+      const userFromDB = (data.find(user => user.username === username))
+      if (userFromDB) {
+        if (userFromDB.password === password) {
+          res.status(200).json({
+            message: 'Login successful',
+            path: '/details'
+          });
+        } else {
+          res.status(201).json({
+            message: 'Wrong password',
+          });
+        }
+      } else {
+        res.status(202).json({
+          message: 'Invalid username',
+          path: '/register'
+        });
+      }
+    });
+  } else if (!username || !password) {
+    res.status(403).json({
+      message: 'Missing query'
+    });
+  }
+});
+
+
 app.get('/details', (req, res) => {
   res.sendFile(path.join(__dirname + '/details.html'));
 });
--- Week-10/Day-03/userDetails/static/login_main.js
+++ Week-10/Day-03/userDetails/static/login_main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log('login');
+
+
+const form = document.querySelector('form');
+const { username, password } = form.elements;
+
+const sendHTTPRequest = (method, url, callback) => {
+  const xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.send();
+  xhr.onload = (response) => {
+    if (xhr.status === 200 && response) {
+      data = JSON.parse(response);
+      callback(response);
+    }
+  }
+}
+
+sendHTTPRequest('GET', '/user', () => {
+});
